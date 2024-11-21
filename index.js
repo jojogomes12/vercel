ce4b7c38bd04995e10d8554306fc7ebc06bd3e61
jojogomes12 <joaoecarol39@gmail.com>
2024-11-21 18:03:01 -0300

    Sua mensagem de commit aqui

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.get('/test-db', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+    //console.log(`Server listening at http://localhost:${port}`);
 });
